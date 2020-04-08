12c583b1416e1d77d4794af8c09f1601f0670a94
pierre-1 <pierreantoine.tiberi@gmail.com>
2020-04-08 16:58:03 +0200

    Refactors app structure, adds show action

---
+++ controllers/booksController.js
@@ -0,0 +1,21 @@
+const models = require("../database/models");
+
+const booksController = {
+  async index(req, res, next) {
+    const booksIndex = await models.Book.findAll();
+    res.json({ books: booksIndex });
+  },
+
+  async show(req, res, next) {
+    const { id } = req.params;
+    const book = await models.Book.findByPk(id);
+    res.json({ book: book });
+  }
+};
+
+// const index = async (req, res, next) => {
+//   const booksIndex = await models.Book.findAll();
+//   res.json({ books: booksIndex });
+// };
+
+module.exports = booksController
--- specs/booksEndpoint.spec.js
+++ specs/booksEndpoint.spec.js
@@ -1,29 +1,48 @@
-const app = require('../app')
-const supertest = require('supertest')
-const expect = require('chai').expect
-const jsonResponse = require('./jsonResponse')
-let server, request, response
+const app = require("../app");
+const supertest = require("supertest");
+const expect = require("chai").expect;
+const jsonResponse = require("./jsonResponse");
+let server, request, response;
 
-before((done) => {
-    server = app.listen(done)
-    request = supertest.agent(server)
-})
+before(done => {
+  server = app.listen(done);
+  request = supertest.agent(server);
+});
 
-after((done) => {
-    server.close(done)
-})
+after(done => {
+  server.close(done);
+});
 
-describe('GET /api/v1/books', () => {
-    before(async () => {
-        response = await request.get('/api/v1/books')
-    })
+describe("GET /api/v1/books", () => {
+  before(async () => {
+    response = await request.get("/api/v1/books");
+  });
 
-    it('responds with a status 200', () => {
-        expect(response.status).to.equal(200)
-    })
-    it('responds with list of books', () => {
-        const expectedBody = { books: [{ title: 'The Language Instinct'}] }
-        expect(jsonResponse(response))
-        .to.equal(JSON.stringify(expectedBody))
-    })
-})
+  it("responds with a status 200", () => {
+    expect(response.status).to.equal(200);
+  });
+  // it('responds with list of books as an array', () => {
+  //     // const expectedBody = { books: [{ title: 'The Language Instinct'}] }
+  //     expect(Array.isArray(response.body.books))
+  //     .to.equal(true)
+  // })
+  it("responds with list of books as an array", () => {
+    // const expectedBody = { books: [{ title: 'The Language Instinct'}] }
+    console.table(response.body.books);
+    expect(response.body.books).to.be.an("array");
+  });
+
+  it("returns title for books", () => {
+    expect(response.body.books[0].title).to.equal("Learn NodeJS with Thomas");
+  });
+});
+
+describe("GET /api/v1/books/:id", () => {
+  before(async () => {
+    response = await request.get("/api/v1/books/1");
+  });
+
+  it("responds with a single book", () => {
+    expect(response.body.book.id).to.equal(1);
+  })
+});
